e0a1fdd971bcb7a53a09d507763bb188e5c44884
luketucich <ltucich@gmail.com>
2024-10-28 12:19:37 -0400

    Completed merge sort function.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("test");
+function merge(left, right) {
+  const sorted = [];
+
+  while (left.length > 0 && right.length > 0) {
+    sorted.push(left[0] <= right[0] ? left.shift() : right.shift());
+  }
+
+  return sorted.concat(left, right);
+}
+
+function mergeSort(arr) {
+  if (arr.length == 1) return arr;
+
+  const mid = Math.ceil(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, arr.length);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
